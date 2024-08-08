9885eb3961a28265ee76f7f3d8f42cb1bdb0ff9c
Jeron Cansanay <jeronscc@gmail.com>
2024-08-08 13:32:20 +0800

    1:32 8/8

--- pages/dahsboard.php
+++ pages/dahsboard.php
@@ -17,7 +17,7 @@
     <?php include '/xampp/htdocs/capstone/src/components/moderator_navbar.php'; ?>
     <div class="container-fluid main-content">
         <div class="row">
-            <div class="h1 col-sm-6 col-md-6 text-start">
+            <div class="h3 col-sm-6 col-md-6 text-start">
                 Dashboard
             </div>
             <div class="col-sm-6 col-md-6 d-flex justify-content-end">
@@ -38,7 +38,7 @@
                     </div>
                 </div>
     </div>
-
+    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- src/components/moderator_navbar.php
+++ src/components/moderator_navbar.php
@@ -1,7 +1,7 @@
-<div class="sidebar p-3">
+<!-- Sidebar for larger screens -->
+<div class="sidebar p-3 d-none d-md-block">
     <div class="d-flex align-items-center">
-        <img src="/capstone/src/assets/kayanlog-logo.png" alt="Logo"
-            style="width: 80px; height: 80px; margin-right: 10px;">
+        <img src="/capstone/src/assets/kayanlog-logo.png" alt="Logo" style="width: 80px; height: 80px; margin-right: 10px;">
         <h5 class="mb-0">Barangay Kay-Anlog</h5>
     </div>
     <ul class="nav flex-column mt-3">
@@ -9,8 +9,7 @@
             <a class="nav-link" href="#"><i class="fas fa-tachometer-alt me-2"></i> Dashboard</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link d-flex" data-bs-toggle="collapse" href="#residentRecords" aria-expanded="false"
-                aria-controls="residentRecords">
+            <a class="nav-link d-flex" data-bs-toggle="collapse" href="#residentRecords" aria-expanded="false" aria-controls="residentRecords">
                 <i class="fas fa-users me-2"></i>Resident Records
                 <i class="fas fa-chevron-down" style="margin-left: auto;"></i>
             </a>
@@ -39,6 +38,45 @@
         </li>
     </ul>
 </div>
+
+<!-- Top Navbar for smaller screens -->
+<nav class="navbar navbar-expand-md navbar-dark bg-primary d-md-none fixed-top">
+    <a class="navbar-brand" href="#">
+        <img src="/capstone/src/assets/kayanlog-logo.png" alt="Logo" style="width: 40px; height: 40px;">
+        Barangay Kay-Anlog
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-tachometer-alt me-2"></i> Dashboard</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fas fa-users me-2"></i> Resident Records
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#"><i class="fas fa-list me-2"></i> Resident List</a></li>
+                    <li><a class="dropdown-item" href="#"><i class="fas fa-plus me-2"></i> Add Records</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-file-alt me-2"></i> Reports</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-book me-2"></i> Blotter Records</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-question-circle me-2"></i> F.A.Q.s</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-sign-out-alt me-2"></i> Sign out</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
