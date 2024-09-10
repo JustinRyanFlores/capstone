f3287f1e6125ae5dd9746bdae3b41a932d67166c
JustinRyanFlores <21-68575@g.batstate-u.edu.ph>
2024-09-10 21:34:21 +0800

    test

---
+++ pages/resident_list.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <title>My Web Application</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="/capstone/src/css/navbar.css" />
+    <link rel="stylesheet" href="/capstone/src/css/header.css" />
+    <?php include '/xampp/htdocs/capstone/src/components/header.php'; ?>
+</head>
+
+<body>
+    <?php include '/xampp/htdocs/capstone/src/components/moderator_navbar.php'; ?>
+    <div class="container-fluid main-content">
+        <div class="row">
+            <div class="h3 col-sm-6 col-md-6 text-start h5-sm">
+                Dashboard
+                <div class="h6" style="font-style: italic; color: grey">
+                    Home/Dashboard
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 d-flex justify-content-sm-between justify-content-md-end ">
+                <div>
+                    <?php displayDateTime(); ?>
+                </div>
+            </div>
+        </div>
+                <!-- Add your main content here -->
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6 text-left">
+                            col 1
+                        </div>
+                        <div class="col-sm-6">
+                            col 2
+                        </div>
+                    </div>
+                </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
