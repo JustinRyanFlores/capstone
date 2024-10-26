bdb288a5a206defb93c3e490eec728cc2d0dcbf4
Justin Flores <21-68575@g.batstate-u.edu.ph>
2024-10-26 13:06:08 +0800

    div remove resident list

--- pages/resident_list.php
+++ pages/resident_list.php
@@ -365,9 +365,6 @@ if (isset($_GET['search'])) {
             }
         </script>
     </div>
-    </div>
-    </div>
-    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
